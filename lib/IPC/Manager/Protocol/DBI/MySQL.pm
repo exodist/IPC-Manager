9dd8ca162a50e2c66a9ffa914a2f98f7e4f25506
Chad Granum <exodist7@gmail.com>
2025-12-28 00:26:33 -0800

    Add MySQL

---
+++ lib/IPC/Manager/Protocol/DBI/MySQL.pm
@@ -0,0 +1,68 @@
+package IPC::Manager::Protocol::DBI::MySQL;
+use strict;
+use warnings;
+
+use Carp qw/croak/;
+use File::Temp qw/tempdir/;
+use IPC::Manager::Util qw/pid_is_running/;
+
+use DBI;
+
+use parent 'IPC::Manager::Protocol::DBI';
+use Object::HashBase qw{
+    +QDB
+};
+
+sub ready { 1 }
+
+sub dsn { $_[0]->{+INFO} }
+
+sub escape { '`' }
+
+sub default_attrs { +{ AutoCommit => 1 } }
+
+sub table_sql {
+    return (
+        <<"        EOT",
+            CREATE TABLE IF NOT EXISTS ipcm_clients(
+                `id`        VARCHAR(36)     NOT NULL PRIMARY KEY,
+                `pid`       INTEGER         NOT NULL
+            );
+        EOT
+        <<"        EOT",
+            CREATE TABLE IF NOT EXISTS ipcm_messages(
+                `id`        VARCHAR(36)     NOT NULL PRIMARY KEY,
+                `to`        VARCHAR(36)     NOT NULL REFERENCES ipcm_clients(id) ON DELETE CASCADE,
+                `from`      VARCHAR(36)     NOT NULL,
+                `stamp`     DOUBLE          NOT NULL,
+                `content`   BLOB            NOT NULL,
+                `broadcast` BOOL            NOT NULL DEFAULT FALSE
+            );
+        EOT
+    );
+}
+
+sub listen {
+    my $class = shift;
+    my (%params) = @_;
+
+    my $dsn = $params{info};
+
+    unless ($dsn) {
+        require DBIx::QuickDB;
+        my $qdb = DBIx::QuickDB->build_db(m_db => {driver => 'MySQL'});
+        $params{+QDB}  = $qdb;
+        $params{+INFO} = $qdb->connect_string;
+        $params{+USER} = $qdb->username;
+        $params{+PASS} = $qdb->password;
+    }
+
+    $class->new(%params, ID() => 'manager', IS_MANAGER() => 1, MANAGER_PID() => $$);
+}
+
+sub manager_cleanup_hook {
+    my $self = shift;
+    delete $self->{+QDB};
+}
+
+1;
